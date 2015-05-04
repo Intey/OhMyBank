bf8cb94fda457df1648ea79b271c7710c841cd54
Intey <ziexe0@gmail.com>
2015-05-04 09:06:05 +0000

    Add handler for login.

--- src/ombs/handler.clj
+++ src/ombs/handler.clj
@@ -11,19 +11,29 @@
   "Handler. show index page"
   (view/index ctxt))
 
-(defn user [params]
-  "Generate user page, with his name and events."
-  (view/user (:username params))
-  )
-
 (defn regpage [_] (view/register {}))
+
+(defn log-user [uname]
+  "Generate user page, with his name and events."
+  (sess/put! uname)
+  (redirect "/")  
+  )
 
 (defn register [params]
   (if (core/reg-ok? (params "username") (params "pass1") (params "pass2"))  
-    (do    
+    ;true
+    (do 
       (db/add-user (params "username") (params "pass1") (params "birthdate") (core/rate (params "student-flag")))
-      (sess/put! :username (params "username"))
-      (redirect "/") )
+      (log-user (params "username")) )
+    ;fasle 
     (view/register params) 
     ))
+
+
+(defn login [{{uname :username pass :password} :params}]
+  (if  (= pass (:password (db/get-user uname)))
+    (log-user uname)
+    (view/index {:error "wrong login/pass"})
+    )
+  )
 
