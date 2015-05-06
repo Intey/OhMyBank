bb66b066a53e307c8e5972770bd054ad751728c2
Intey <ziexe0@gmail.com>
2015-05-06 00:44:29 +0000

    Fix: params keywords, logining, log-up'ing.

--- src/ombs/view.clj
+++ src/ombs/view.clj
@@ -6,12 +6,17 @@
 
     ) )
 
-(h/deftemplate index "ombs/index.html" [& ctxt])
+(h/deftemplate index "ombs/index.html" [ctxt]
+  [:#error] (fn [match] 
+              (if-let [error (:error ctxt)]
+                ((h/content error)      match)  
+                ((h/content "") match) )))
 
 ;"Generate register page. If in given params founded keys for this page - fill fields with 
 ;founded values"
 (h/deftemplate register "ombs/register.html" [params] 
   [:#uname] (h/content (sess/get :username "no user"))
+  [:#error] (h/content (str params))
   [:#username]     (h/set-attr :value (params "username"))
   [:#birthdate]    (h/set-attr :value (params "birthdate"))
   [:#student-flag] (if (not-empty (params "student-flag")) 
