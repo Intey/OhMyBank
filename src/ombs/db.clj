5a97d4a5916ef93328d92f30898f3c4ebcac0481
Intey <ziexe0@gmail.com>
2015-05-14 22:10:24 +0000

    Pretty code.

--- src/ombs/db.clj
+++ src/ombs/db.clj
@@ -9,16 +9,13 @@
 ; about many-to-many https://groups.google.com/d/msg/sqlkorma/r3kR6DyQZHo/RrQS_J8kkQ8J
 
 (declare events)
-(sql/defentity 
-  users
+(sql/defentity users
   (sql/many-to-many events :participants {:lfk :uid :rfk :eid}))
 
-(sql/defentity 
-  events
+(sql/defentity events
   (sql/many-to-many users :participants {:lfk :eid :rfk :uid}))
 
-(sql/defentity 
-  participants
+(sql/defentity participants
   (sql/belongs-to events {:fk :eid})
   (sql/belongs-to users {:fk :uid}))
 
@@ -84,5 +81,4 @@
     (do 
       (sql/insert participants (sql/values {:uid (get-uid uname) :eid (get-eid ename)}))
       "Success." ) 
-    (str uname " already participate " ename " event."))
-  )
+    (str uname " already participate " ename " event.")) )
