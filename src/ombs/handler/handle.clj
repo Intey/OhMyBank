547276d525bc775aeb74b66b14f86a5a0de6ea99
Intey <ziexe0@gmail.com>
2015-08-15 22:53:25 +0000

    add error field on user page, use when, instead if.

--- src/ombs/handler/handle.clj
+++ src/ombs/handler/handle.clj
@@ -27,15 +27,14 @@
   (let [uname (sess/get :username)
         uid (db/get-uid uname)
         eid (db/get-eid ename date)]
-    (if (isvalid/ids? eid uid)
-      (db/debit-payment uid eid (db/get-debt uname ename date))
-      ))
+    (when (isvalid/ids? eid uid) 
+      (db/debit-payment uid eid (db/get-debt uname ename date)) ))
   (redirect "/user")) ; go to user page in any case
 
 (defn participate [{ename :event-name date :date price :price }]
   "Add participation of current user and selected event(given as param from post)"
   (let [uname (sess/get :username)]  
-    (if (isvalid/participation? ename date uname)
+    (when (isvalid/participation? ename date uname)
       (core/add-participant ename date uname)))
   (redirect "/user")); go to user page in any case
 
