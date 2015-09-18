1b5cf6dde609615b912a9142c3f2043435f29c41
Intey <ziexe0@gmail.com>
2015-09-18 22:42:55 +0000

    [fix] Error messages.

--- src/ombs/handler/eventacts.clj
+++ src/ombs/handler/eventacts.clj
@@ -5,6 +5,7 @@
     [ombs.db.payment :as dbpay]
     [ombs.validate :as isvalid]
     [ombs.funcs :refer [parse-int]]
+    [ombs.handler.pages :as pages]
     [noir.session :as sess]
     [noir.response :refer [redirect] ]
     ))
@@ -16,7 +17,6 @@
 (defn pay [{ename :event-name date :date :as params}]
   "Add participation of current user and selected event(given as param from 
   post). Parts in params is count of parts, that user want to pay"
-  (println "call pay with: " ename " " date)
   (let [uname (sess/get :username)
         uid (db/get-uid uname)
         eid (db/get-eid ename date)]
@@ -24,12 +24,11 @@
       (dbpay/debit-payment uid eid (dbpay/get-debt uname ename date)) 
       (if (db/can-finish? ename date) 
         (finish ename date)))) 
-  (redirect "/user")) ; go to user page in any case
+  (pages/user)) ; go to user page in any case
 
 (defn pay-part [{ename :event-name date :date parts :parts :as params}]
   "Add participation of current user and selected event(given as param from 
   post). Parts in params is count of parts, that user want to pay"
-  (println "call pay-parts with: " ename " " date " '" parts "'")
   (let [uname (sess/get :username)
         uid (db/get-uid uname)
         eid (db/get-eid ename date)
@@ -40,16 +39,14 @@
         (dbpay/debit-payment uid eid (dbpay/get-debt uname ename date))) 
       (when (= 0 (db/get-rest-parts ename date))
         (finish ename date) ))) 
-  (redirect "/user")) ; go to user page in any case
+  (pages/user)) ; go to user page in any case
 
 (defn process-it [ename date parts uname]
-  (println "processing " ename " " date " " parts " " uname)
   (if (isvalid/parts? ename date parts) ; its check if parts >= than free parts
     (do 
       (dbpay/debit-payment (db/get-uid uname) (db/get-eid ename date) (core/parts-price ename date parts))
       (db/shrink-goods ename date parts))
-    (redirect "/user")
-    ))
+    (pages/user)))
 
 (defn participate [{ename :event-name date :date}]
   "Add participation of current user and selected event(given as param from
@@ -57,9 +54,9 @@
   (let [uname (sess/get :username)]  
     (when (isvalid/participation? ename date uname)
       (core/add-participant ename date uname)))
-  (redirect "/user")); go to user page in any case
+  (pages/user)); go to user page in any case
 
 (defn start [{ename :event-name date :date}]
   ;isvalid: not started, exists.
   (core/start-event ename date)
-  (redirect "/user")); go to user page in any case 
+  (pages/user)); go to user page in any case 
