21f45a2e7ad5e72a72dd4c79100652b22d29465a
Intey <ziexe0@gmail.com>
2015-10-02 21:32:36 +0000

    [ref] Move haml files to src.

--- hamly.sh
+++ hamly.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 hamle(){
-    haml "resources/src/$1.haml" "resources/public/$1.html"
+    haml "src/haml/$1.haml" "resources/public/$1.html"
     if [[ $? != 0 ]]; then
         exit 1;
     fi
 }
 
-for file in $(ls resources/src); do
+for file in $(ls src/haml); do
     hamle "${file%.*}"
 done
