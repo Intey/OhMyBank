522f0c36f490caaa2402bc24cc98c6052d717461
Intey <ziexe0@gmail.com>
2015-10-04 20:47:30 +0000

    [rex] 80 columns rule.

--- src/ombs/db/payment.clj
+++ src/ombs/db/payment.clj
@@ -5,9 +5,12 @@
             ))
 
 (defn participated?
-  "Check participation in event. If user have some payment action on event - he is participated."
+  "Check participation in event. If user have some payment action on event -
+  he is participated."
   ([uid eid]
-   (not (empty? (sql/select participation (sql/where (and (= :users_id uid) (= :events_id eid)))))))
+   (not (empty? (sql/select participation
+                            (sql/where {:serds_id uid
+                                        :events_id eid})))))
 
   ([uname ename edate]
    (participated? (get-uid uname) (get-eid ename edate))))
@@ -16,42 +19,54 @@
   "sum of all credits and debits in pays for current user and event. "
   ; full debt
   ([username]
-   (if-let [summary (:debt (first (sql/select debts (sql/where {:user username}) (sql/aggregate (sum :debt) :debt))))]
+   (if-let [summary (:debt (first (sql/select debts
+                                              (sql/where {:user username})
+                                              (sql/aggregate (sum :debt)
+                                                             :debt))))]
      summary
      0.0))
   ; debt on some event
   ([username event date]
-   (if-let [summary (:debt (first (sql/select debts (sql/where {:user username :event event :date date}))))]
+   (if-let [summary (:debt (first (sql/select debts
+                                              (sql/where {:user username
+                                                          :event event
+                                                          :date date}))))]
      summary
      0.0))
   ([uid eid]
-   (if-let [summary (:debt (first (sql/select debts (sql/where {:eid eid :uid uid}))))]
+   (if-let [summary (:debt (first (sql/select debts
+                                              (sql/where {:eid eid
+                                                          :uid uid}))))]
      summary
-     0.0)) 
+     0.0))
   )
 
 (defn credit-payment [eid uid money]
   (println (str "credit " uid " user " uid " money " money ))
-  (sql/insert pays (sql/values { :users_id uid :events_id eid :credit money })))
+  (sql/insert pays (sql/values {:users_id uid
+                                :events_id eid
+                                :credit money })))
 
 (defn debit-payment [eid uid money]
   (println (str "dedit " uid " user " uid " money " money ))
-  (sql/insert pays (sql/values { :users_id uid :events_id eid :debit money }))
-  )
+  (sql/insert pays (sql/values {:users_id uid
+                                :events_id eid
+                                :debit money })))
 
 (defn add-participant [event date user]
-  (sql/insert participation (sql/values {:events_id (get-eid event date) :users_id (get-uid user)})))
+  (sql/insert participation (sql/values {:events_id (get-eid event date)
+                                         :users_id (get-uid user)})))
 
 (defn get-participants [ename edate]
   (mapv #(first (vals %)) (sql/select participants (sql/fields :user)
                                       (sql/where {:event ename :date edate}))))
 
 (defn calc-fee-money [uid eid parts]
-  (if (> parts 0) 
+  (if (> parts 0)
     (parts-price eid parts)
     (get-debt uname ename date)) )
 
-(defn create-fee [uname ename date & [parts]] 
+(defn create-fee [uname ename date & [parts]]
   (let [uid (get-uid uname)
         eid (get-eid ename date) ]
     (sql/insert fees (sql/values {:users_id uid
