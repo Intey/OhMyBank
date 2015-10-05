6a26d2bb6f39b3abfc8b859e1ffe2d80aa486940
Intey <ziexe0@gmail.com>
2015-10-05 00:57:33 +0000

    [fix][tests] Seems that can-pay work ok for solid events.

--- spec/tests/eventacts/solid.clj
+++ spec/tests/eventacts/solid.clj
@@ -12,15 +12,15 @@
 (spec/describe "Pay button on solid event(without parts)"
                (spec/before-all
                  (println "############################## RESET DB #######################################")
-                 (sh "bash" "-c" "./resetdb.sh test")
-                 )
-               (spec/it "is hidden, when user NOT participate event"
+                 (sh "bash" "-c" "./resetdb.sh test"))
+
+               (spec/it "is HIDDEN, when user NOT participate event"
                         (spec/should-not (dbp/can-pay? uid eid-solid)))
 
-               (dbp/add-participant uid eid-solid)
-               (core/start-event eid-solid)
-
-               (spec/it "is visible, when user participated and fees sum not cover party-pay"
+               (spec/it "is VISIBLE, when user participated and fees sum NOT cover party-pay"
+                        (nil? (println "Participate and start event " (db/get-event eid-solid)))
+                        (dbp/add-participant uid eid-solid)
+                        (core/start-event eid-solid)
                         (spec/should (dbp/can-pay? uid eid-solid)) )
                (dbp/create-fee uid eid-solid))
 
