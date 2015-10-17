11d8aa50962b31e32b0bc2b0c5ee3e4fcf422b95
Intey <ziexe0@gmail.com>
2015-10-17 21:24:18 +0000

    [fix] Work with not only clj.

--- endlinefix.sh
+++ endlinefix.sh
@@ -2,5 +2,13 @@ if [[ -z $1 ]]; then
     echo "need filename"
     exit 1
 fi
+if [[ -f $1 ]]; then
+    sed -i "s/\s\+$//" $1
+    exit 0
+fi
+if [[ -d $1 ]]; then
+    find $@ -iname "*clj" | xargs sed -i "s/\s\+$//"
+    echo "process dir $1"
+    exit 0
+fi
 
-find $@ -iname "*clj" | xargs sed -i "s/\s\+$//"
