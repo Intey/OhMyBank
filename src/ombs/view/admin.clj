01688c07c0a8a1122b29be3eef8645f9be24aeac
Intey <ziexe0@gmail.com>
2015-10-17 23:01:52 +0000

    [up] Use admin page for admin; Fees now is table.

--- src/ombs/view/admin.clj
+++ src/ombs/view/admin.clj
@@ -9,10 +9,10 @@
     [ombs.view.fees :refer [fee-elem]]
     ))
 
-(h/deftemplate page "../resources/public/user.html" [username]
+(h/deftemplate page "../resources/public/admin.html" [username]
   [:#user] (h/content username)
   [:#error] (h/content (errors-string))
   [:.debt] (h/content (str (core/debt username)))
   [:section.events :> :article] (h/content (map #(event-elem %) (core/events)))
-  [:section.fees :> :article] (h/content (map #(fee-elem %) (get-fees)))
+  [:section :> :table :> :tbody] (h/content (map #(fee-elem %) (get-fees)))
   )
