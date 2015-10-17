7f76f2e3f37e89c7e26596d9abf0943a22f9724d
Intey <ziexe0@gmail.com>
2015-10-17 21:12:57 +0000

    Try to fix autostart test in strange cases with lein ring server.

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0" ]
-                           ;[org.clojure/core.memoize "0.5.7"] ; noir-middleware dependence
-                           ;[org.clojure/math.numeric-tower "0.0.4"] ; math funcs
                            [org.clojure/clojure-contrib "1.2.0"] ;additions
                            [ring/ring-jetty-adapter "1.3.2"]
                            [compojure "1.3.3"] ; routing
@@ -14,7 +12,6 @@
                            [liberator "0.12.2"] ; for RESTful project
                            [cheshire "5.4.0"] ; JSON parsing
                            [clj-time "0.10.0"]
-
                            [org.clojure/java.jdbc "0.3.6"] ; deps fo db-driver
                            [org.xerial/sqlite-jdbc "3.8.7"] ; sqlite driver
                            [korma "0.4.0"] ; sql in code
@@ -22,27 +19,37 @@
 
 
 
-                           [speclj "3.2.0"] ; bdd tests - core
-                           [clj-webdriver "0.6.1" ; this use old version of selenium-server, so
-                            :exclusions [org.seleniumhq.selenium/selenium-server]]
-                           [org.seleniumhq.selenium/selenium-server "2.45.0"] ; drive it manualy
-                           [ring-mock "0.1.5"] ; ??
-                           [ring/ring-jetty-adapter "1.3.2"] ; for runs app before tests
                            ]
+            :profiles {
+                       :dev { 
+                             :dependencies [
+                                            [speclj "3.3.0"]
+                                            [clj-webdriver "0.6.1" ; this use old version of selenium-server, so
+                                             :exclusions [org.seleniumhq.selenium/selenium-server]]
+                                            [org.seleniumhq.selenium/selenium-server "2.45.0"] ; drive it manualy
+                                            [ring-mock "0.1.5"] ; ??
+                                            [ring/ring-jetty-adapter "1.3.2"] ; for runs app before tests
+                                            ]}
 
+                       }
+
+            :ragtime {:migrations ragtime.sql.files/migrations
+                      :database "jdbc:sqlite:database.db" }
+
+            :ring {:handler ombs.route/engine
+                   ;:init ombs.handler/init       ; use this, Luke.
+                   ;:destroy ombs.handler/destroy
+                   } 
 
             :plugins [ [lein-ring "0.8.8"]
+                      [speclj "3.3.0"]
                       [lein-ancient "0.6.6"]
                       [ragtime/ragtime.lein "0.3.8"]
-                      [speclj "3.2.0"] 
                       [lein-gossip "0.1.0-SNAPSHOT"]
                       [cider/cider-nrepl "0.9.1"]
                       ]
 
+            :eval-in :nrepl
             :test-paths ["spec"]
-            :ragtime {:migrations ragtime.sql.files/migrations
-                      :database "jdbc:sqlite:database.db" }
-            :ring {:handler ombs.route/engine
-                   ;:init ombs.handler/init       ; use this, Luke.
-                   ;:destroy ombs.handler/destroy
-                   })
+
+            )
