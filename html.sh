1c3a43a3cc3d3d7f10664515e3b7fb8b3d9942f5
Intey <ziexe0@gmail.com>
2015-10-23 19:37:56 +0000

    [fix] Grunt, boot...bash script!

--- html.sh
+++ html.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+# 2 cycles faster
 
-jade $(find src/html -iname "*.jade") --out "resources/public"
+jade $(find src/html -iname "*.jade") -P --out "resources/public"
+
+files=($(find src/html -iname "*.jade"))
+for f in ${files[@]}; do
+    bname=$(basename $f)
+    touch -c "src/ombs/view/${bname%.*}.clj"
+done
