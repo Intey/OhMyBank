507f00d83a2a9158cb9428f9115ac47894d45286
Intey <ziexe0@gmail.com>
2015-11-08 17:57:42 +0000

    Extract actions.

---
+++ src/ombs/view/actions.clj
@@ -0,0 +1,35 @@
+(ns ombs.view.actions
+  (:require
+    [net.cgrand.enlive-html :as h]
+    [noir.session :as sess]
+    [ombs.core :as core]
+    )
+  )
+(defn fill-parts [parts match]
+  (if (= 0 parts)
+    nil
+    ((h/content (parts-snip parts)) match)))
+
+(defn pay [name date match]
+  (if (and
+        (core/participated? (sess/get :username) name date)
+        (core/is-active? name date) ) ; in-progress
+    ; add parts field
+    ((h/content "Pay") ((rm-attr-class "disabled")   match))
+    ((h/content "Pay") ((set-attr-class "disabled")  match))
+    ))
+
+(defn start [name date author match]
+  (if (and (= author (sess/get :username))
+           (core/is-initial? name date)
+           (> (core/participants-count name date) 0))
+    ((h/remove-class "disabled") match)
+    ((h/add-class "disabled")    match)))
+
+(defn participate [name date status match]
+  (if (and
+        (not (core/participated? (sess/get :username) name date))
+        (not= status "finished"))
+    ((h/remove-attr "disabled") match)
+    ((h/add-class "disabled")   match)))
+
--- src/ombs/view/event.clj
+++ src/ombs/view/event.clj
@@ -7,6 +7,7 @@
     [ombs.core :as core]
     [ombs.validate :refer [errors-string]]
     [ombs.view.dom :refer [set-attr-class rm-attr-class content-wrap]]
+    [ombs.view.actions :as actions]
     ))
 
 (h/defsnippet action "../resources/public/action.html" [:.action] [{:keys [money button]} match]
@@ -14,8 +15,6 @@
   [:button] button)
 
 (declare get-action)
-(declare get-button)
-
 (h/defsnippet event-elem "../resources/public/event.html" [:.event2] [{:keys [name price date author status parts] :as event}]
   [:.name]   (h/content (str author "'s " name))
   [:.date]   (h/content (str date))
@@ -24,58 +23,12 @@
 (h/defsnippet parts-snip "../resources/public/parts.html" [:#parts-row] [parts]
   [:.parts] (h/set-attr :value (str parts)))
 
-
-(defn get-money [{:keys [name price date author status parts]}]
-  "Return price or user debt, depends on user participation.
-  If user not participate - show price; else - show debt.
-  Also, if user have fee on this event, or event is finished - return nil"
-  (when-let [uname (sess/get :username)]
-    (if (and
-          (core/participated? uname name date)
-          (core/is-active? name date) ) ; have debt
-      (partial content-wrap (core/debt uname name date))
-      (partial content-wrap price)
-      )))
-
-(defn fill-parts [parts match]
-  (if (= 0 parts)
-    nil
-    ((h/content (parts-snip parts)) match)))
-
-(defn pay-action [name date match]
-  (if (and
-        (core/participated? (sess/get :username) name date)
-        (core/is-active? name date) ) ; have debt
-    ; add parts field
-    ((h/content "Pay") ((rm-attr-class "disabled")   match))
-    ((h/content "Pay") ((set-attr-class "disabled")  match))
-    ))
-
-(defn start-action [name date author match]
-  (if (and (= author (sess/get :username))
-           (core/is-initial? name date)
-           (> (core/participants-count name date) 0))
-    ((h/remove-class "disabled") match)
-    ((h/add-class "disabled")    match)))
-
-(defn participate-action [name date status match]
-  (if (and
-        (not (core/participated? (sess/get :username) name date))
-        (not= status "finished"))
-    ((h/remove-attr "disabled") match)
-    ((h/add-class "disabled")   match)))
-
-(defn get-action [event]
-  "return hashmap with function for money and button"
-  {:money (get-money event)
-   :button (get-button :pay event) }
-  )
-
 (defn get-button [button-type {:keys [name price date author status parts]}]
   (case button-type
-    :pay          (partial pay-action name date)
-    :start        (partial start-action name date author)
-    :participate  (partial participate-action name date status))
+    :pay          (partial actions/pay name date)
+    :start        (partial actions/start name date author)
+    :participate  (partial actoons/participate name date status))
+    :finished     (partial content-wrap "Finished")
   )
 
 
