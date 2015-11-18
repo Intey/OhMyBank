40ff6249a2d20b9fdab9c2ef6a397a28e8d04976
Intey <ziexe0@gmail.com>
2015-11-18 23:36:30 +0000

    [up] Add function for actions.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,30 @@
-function affirm(b) {
-    var id = $(b).parent().parent().attr('id');
+function affirm(elem) {
+    var id = $(elem).parent().parent().attr('id');
     $.get( '/affirm', {fid: id} )
         .done( function(data) {
-            alert(data);
+            $(elem).remove();
         });
+}
+function start(elem) {
+    var eid = $(elem).parent().parent().attr('id');
+    $.get( '/start', {uid: uid, eid: eid} )
+        .done( function(data) {
+            $(elem).remove();
+        });
+}
+function pay(elem) {
+    var eid = $(elem).parent().parent().attr('id');
+    $.get( '/pay', {uid: uid, eid: eid} )
+        .done( function(data) {
+            $(elem).remove();
+        });
+}
+
+function participate(elem) {
+    var eid = $(elem).parent().parent().attr('id');
+    $.get( '/participate', {uid: uid, eid: eid} )
+        .done( function(data) {
+            $(elem).remove();
+        });
+
 }
