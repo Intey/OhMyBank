db5cb4cbf63eda6b0059030fde2e2d751d2ed655
Intey <ziexe0@gmail.com>
2015-12-01 21:44:25 +0000

    Attack onclick JS call

--- src/ombs/view/actions.clj
+++ src/ombs/view/actions.clj
@@ -3,18 +3,22 @@
     [net.cgrand.enlive-html :as h]
     [noir.session :as sess]
     [ombs.core :as core]
-    [ombs.view.dom :as dom]
-    )
-  )
+    [ombs.view.dom :as dom]))
 
 (h/defsnippet parts-snip "../resources/public/parts.html" [:#parts-row] [parts]
   [:.parts] (h/set-attr :value (str parts)))
+
 (defn fill-parts [parts match]
   (if (= 0 parts)
     nil
     ((h/content (parts-snip parts)) match)))
 
-(defn pay [match] ((h/content "Pay") match))
-(defn start [match] ((h/content "Start") match))
-(defn participate [match] ((h/content "Participate") match))
+(defn pay [match]
+  ((comp (h/set-attr :onclick "pay(this)") (h/content "Pay")) match))
+
+(defn start [match]
+  ((comp (h/set-attr :onclick "start(this)") (h/content "Start")) match))
+
+(defn participate [match]
+  ((comp (h/set-attr :onclick "participate(this)") (h/content "Participate")) match))
 
