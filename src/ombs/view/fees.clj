44b8247646b20a61fa8e58eedb953f84ae3165a7
Intey <ziexe0@gmail.com>
2015-12-02 20:58:58 +0000

    [fix] class-names conllision.

--- src/ombs/view/fees.clj
+++ src/ombs/view/fees.clj
@@ -1,5 +1,5 @@
 (ns ombs.view.fees
-  (:require 
+  (:require
     [net.cgrand.enlive-html :as h]
     [net.cgrand.reload :as reload]
     ))
@@ -7,8 +7,8 @@
 (h/defsnippet fee-elem "../resources/public/fee.html" [:.fee] [{:keys [id user event edate date money parts]}]
   [:.fee] (h/set-attr :id id)
   [:.user] (h/content user)
-  [:.event] (h/content (str event " " edate))
-  [:.date] (h/content date)
+  [:.event-name] (h/content (str event " " edate))
+  [:.event-date] (h/content date)
   [:.money] (h/content (str money))
   [:.parts] (h/content (str parts))
   )
