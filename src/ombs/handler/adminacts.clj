effa5692ede85fe0e98b284c9951ac5d6b240d97
Intey <ziexe0@gmail.com>
2015-12-06 17:04:34 +0000

    RESTful affirmation.

--- src/ombs/handler/adminacts.clj
+++ src/ombs/handler/adminacts.clj
@@ -1,7 +1,12 @@
 (ns ombs.handler.adminacts
   (require
     [ombs.db.admin :as db]
-    )
-  )
+    [ombs.validate :as isvalid]
+    [cheshire.core :as ch]
+    ))
 
-(defn affirm [id] (db/affirm id))
+(defn affirm [id]
+  (println "affirm " id)
+  (if (isvalid/fee? id)
+    (db/affirm id)
+    (ch/generate-string {:error (isvalid/errors-string)})))
