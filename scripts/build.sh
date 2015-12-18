d0915b62e2648d94ef862b6050d0865f2eda0dad
Intey <ziexe0@gmail.com>
2015-12-18 20:13:41 +0000

    verbose message for building script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR"/html.sh"
 source $DIR"/js.sh"
+echo "compiling CSS"
 cp "$DIR/../src/css/styles.css" "$DIR/../resources/public/css/"
