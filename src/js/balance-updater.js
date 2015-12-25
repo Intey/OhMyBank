54f0de119fa505ab6518735c3a20d97f95f206d3
Intey <ziexe0@gmail.com>
2015-12-26 00:51:05 +0300

    [fix][moneyout] init balance input; updating.

--- src/js/balance-updater.js
+++ src/js/balance-updater.js
@@ -1,7 +1,14 @@
 
 "use strict";
+(function(){
+    var b = $('.users').children()[0].getAttribute('data-balance');
+    $('.balance').attr({'max': b, 'value': b});
+})()
 
-function update(opt) {
-    var balance = parseInt($(opt).getAttribute('data-balance'));
-    $('.balance').value = balance;
+function update(selector) {
+    var balance = selector.options[selector.selectedIndex].getAttribute('data-balance');
+    var b_e = $('.balance');
+    b_e.attr({'max': balance,
+              'value': balance});
+
 }
