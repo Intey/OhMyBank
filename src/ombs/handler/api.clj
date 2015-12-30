6d1cd3abd22aa681f89686960c524d85423ba4a9
Intey <ziexe0@gmail.com>
2015-12-30 18:08:16 +0300

    [up] Fix events api.

---
+++ src/ombs/handler/api.clj
@@ -0,0 +1,24 @@
+(ns ombs.handler.api
+  (:require
+    [clojure.string :as s]
+    [ombs.db.old :as db]
+    [ombs.db.payment :as dbp]
+    [cheshire.core :as json]
+    [ombs.funcs :refer [with-log]]
+    ))
+
+(defn help []
+  (json/generate-string
+    {
+     :help "Print this help"
+     :get-events "Gets json array of events types(in-progress, finished,
+                 initial).  Return events with type in given types."
+     }))
+
+(defn get-events
+  "Gets json array of events types(in-progress, finished, initial).  Return
+  events with type in given types."
+  [types]
+  (if-let [parsed (json/parse-string types true)]
+    (db/get-events (mapv keyword parsed)))
+  (db/get-events))
