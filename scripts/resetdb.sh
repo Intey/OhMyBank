6cc84f3c7efaaf03e966db5a72a7823014486691
Intey <ziexe0@gmail.com>
2015-12-30 23:52:58 +0300

    [clean]

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/sh
 dbname=$1
 if [[ -z $dbname ]]; then
     echo "one parameter - db name"
@@ -9,4 +9,4 @@ dbname=$1
 
 sqlite3 "$dbname".db < migrations/onebig.down.sql
 sqlite3 "$dbname".db < migrations/onebig.up.sql
-    sqlite3 "$dbname".db < migrations/inserts.up.sql
+sqlite3 "$dbname".db < migrations/inserts.up.sql
