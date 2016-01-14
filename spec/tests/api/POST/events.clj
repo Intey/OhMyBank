9607ea748b3c975c67e09a189669cb500aaede3f
Intey <ziexe0@gmail.com>
2016-01-14 21:31:53 +0300

    [up][tests] POST event with participants.

--- spec/tests/api/POST/events.clj
+++ spec/tests/api/POST/events.clj
@@ -16,12 +16,22 @@
             (t/with-all! event {:name "Hookers" :price 6000.0 :date (date)})
 
             (t/context "new event"
-              (t/it (str "should return event row with id")
+              (t/it "should return event row with id"
                   (let [response (select-keys
                                  (engine (apireq :post "/api/events" (json/generate-string @event)))
                                  [:id :name :price :date])]
                     (t/should== @event response)
-                    (t/should-not-be-nil (:id response)))))
+                    (t/should-not-be-nil (:id response))))
+
+              (t/it "with participants SHOULD return event id and notify users"
+                  (let [participats ["Intey" "andreyk"]
+                        response (engine (apireq :post "/api/events"
+                            (json/generate-string
+                              (assoc @event :participants participats))))]
+                    (t/should== participats (get :participants response))
+                    (t/should-not-be-nil (:id response))))
+              )
+
 
             (t/context "array of events"
                        (let [response
@@ -35,3 +45,4 @@
             (t/after-all
               (println "############################## RESET DB #######################################")
               (sh "bash" "-c" "./scripts/resetdb.sh test")))
+
